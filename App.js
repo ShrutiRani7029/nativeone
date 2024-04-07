229620640175a289e0fa2823a1d5621a1d3aded0
@coder shruti <www.meshruti@gmail.com>
2024-04-07 16:57:20 +0530

    props_access_pass

--- App.js
+++ App.js
@@ -3,27 +3,33 @@ import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 
 const App = () => {
-  const [newdata, setNewData] = useState(102);
-  const [name,setName]=useState('shru');
-
-  const fruit = (val) => {
-    setNewData(20); // Update state using setNewData function
-    console.warn(newdata); // This will log 100, not the updated value
-  }
-
-  function test(){
-    setName('ti');
-  }
+  
+  let name="Shru";
+  let age="22";
+  let friend="dev";
 
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>{newdata}</Text>
-      <Text style={{fontsize:30}} >{name}</Text>
-      <Button title='press state' onPress={test} />
-      <Button title='Press Here' onPress={() => fruit('hello')} color={'green'}></Button>
+      <Text style={{fontSize:30}}>Props in React native</Text>
+      <User name={name}/>
+      <User friend={friend}/>
+      <User ag={age}/>
+
+
     </View>
   );
 };
+const User=(props)=>{
+   return (
+    <View style={{backgroundColor:'green', padding: 7}} >
+      <Text style={{fontSize:30}}>{props.name}</Text>
+      <Text style={{fontSize:30}}>{props.friend}</Text>
+      <Text style={{fontSize:30}}>{props.ag}</Text>
+    </View>
+   )
+}
+
+
 
 export default App;
 
