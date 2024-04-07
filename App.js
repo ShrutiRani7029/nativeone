7766487b1cb761f351a6568ceb0b7a6e47dde49b
@coder shruti <www.meshruti@gmail.com>
2024-04-07 14:50:24 +0530

    State

--- App.js
+++ App.js
@@ -4,15 +4,22 @@ import { View, Text, Button } from 'react-native';
 
 const App = () => {
   const [newdata, setNewData] = useState(102);
+  const [name,setName]=useState('shru');
 
   const fruit = (val) => {
     setNewData(20); // Update state using setNewData function
     console.warn(newdata); // This will log 100, not the updated value
   }
 
+  function test(){
+    setName('ti');
+  }
+
   return (
     <View>
       <Text style={{ fontSize: 30 }}>{newdata}</Text>
+      <Text style={{fontsize:30}} >{name}</Text>
+      <Button title='press state' onPress={test} />
       <Button title='Press Here' onPress={() => fruit('hello')} color={'green'}></Button>
     </View>
   );
