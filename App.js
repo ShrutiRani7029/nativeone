697070d7e45420c092d005f33966913fd47732ad
@coder shruti <www.meshruti@gmail.com>
2024-04-07 17:06:15 +0530

    updation using props

--- App.js
+++ App.js
@@ -3,18 +3,14 @@ import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 
 const App = () => {
+  const [name,setName]=useState("sh")
   
-  let name="Shru";
-  let age="22";
-  let friend="dev";
 
   return (
     <View>
       <Text style={{fontSize:30}}>Props in React native</Text>
-      <User name={name}/>
-      <User friend={friend}/>
-      <User ag={age}/>
-
+      <Button title='Update Props' onPress={()=>setName("shru")}/>
+      <User name={name} age={22} friend={"dev"}/>
 
     </View>
   );
@@ -22,9 +18,9 @@ const App = () => {
 const User=(props)=>{
    return (
     <View style={{backgroundColor:'green', padding: 7}} >
-      <Text style={{fontSize:30}}>{props.name}</Text>
-      <Text style={{fontSize:30}}>{props.friend}</Text>
-      <Text style={{fontSize:30}}>{props.ag}</Text>
+      <Text style={{fontSize:30}}>Name: {props.name}</Text>
+      <Text style={{fontSize:30}}>Age: {props.age}</Text>
+      <Text style={{fontSize:30}}>Friend: {props.friend}</Text>
     </View>
    )
 }
